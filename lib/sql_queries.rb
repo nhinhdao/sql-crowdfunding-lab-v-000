8a2c672d165fd7450aa296ceb0cd4339f0214de8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-23 22:11:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,13 +1,16 @@
 # Write your sql queries in this file in the appropriate method like the example below:
 #
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
+# INSERT INTO users (id, name, age) VALUES
+# (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+# INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+# (1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+# INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+# (1, 10.00, 1, 2),
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"select title, amount from projects join pledges on projects.id = pledges.project_id order by title "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
